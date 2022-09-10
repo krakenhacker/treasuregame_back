f724b20620fc8dccd77b7518217192ee95d9d302
krakenhacker <george.sot@windowslive.com>
2022-09-10 11:13:19 +0300

    remove comment-code lines

--- src/main/java/com/example/treasuregame_back/game/GameConfig.java
+++ src/main/java/com/example/treasuregame_back/game/GameConfig.java
@@ -25,16 +25,11 @@ public class GameConfig {
     @Bean
     CommandLineRunner GameCommandLine(GameRepository repository){
         return args -> {
-//            Game game1 = new Game("Test", LocalDateTime.now(), 0.5,10.5,11.5,12.5,13.5);
-//            Game game2 = new Game("1T", LocalDateTime.now(),0.5,11.5,12.5,13.5,14.5);
-//            List<Game> games = new ArrayList<>();
-//            games.add(game1);
-//            games.add(game2);
-//            repository.saveAll(games);
             User user1 = new User("krakenhacker","george.sot@windowslive.com");
             User user2 = new User("garoufalis","george.sot19@gmail.com");
             userRepository.saveAll(Arrays.asList(user1,user2));
-            repository.save(new Game("Test", LocalDateTime.now(), 0.5,10.5,11.5,12.5,13.5, new GameUsers(user1,"test"), new GameUsers(user2,"test")));
+            Game game1 = new Game("Test", LocalDateTime.now(), 0.5,10.5,11.5,12.5,13.5, new GameUsers(user1,"test"), new GameUsers(user2,"test"));
+            repository.save(game1);
         };
     }
 }
